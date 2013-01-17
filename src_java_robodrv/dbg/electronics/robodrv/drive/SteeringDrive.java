ee4a2189c52db18d1ef59e71755bce829fa49f78
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-01-17 20:41:36 +0000

    dashboard widgets extracted into separate classes, drive interfaced created

---
+++ src_java_robodrv/dbg/electronics/robodrv/drive/SteeringDrive.java
@@ -0,0 +1,14 @@
+package dbg.electronics.robodrv.drive;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dmitri
+ * Date: 1/17/13
+ * Time: 10:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SteeringDrive extends RegulatedDrive {
+
+    void getPosition();
+
+}
