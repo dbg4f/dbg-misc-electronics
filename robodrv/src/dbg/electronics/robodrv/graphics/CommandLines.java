b4b2e07861d36d8de600a23bcf2ea3d779343ed4
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-03-16 15:29:58 +0000

    command line, cursor blinking

--- robodrv/src/dbg/electronics/robodrv/graphics/CommandLines.java
+++ robodrv/src/dbg/electronics/robodrv/graphics/CommandLines.java
@@ -1,4 +1,4 @@
-package dbg.electronics.robodrv.graphics.widgets;
+package dbg.electronics.robodrv.graphics;
 
 import dbg.electronics.robodrv.head.MultilineReportable;
 
@@ -7,12 +7,32 @@ import java.awt.event.KeyEvent;
 public class CommandLines implements MultilineReportable {
 
 
+    public static final char CURSOR_CHAR = '#';
+
+    private static final long CURSOR_BLINK_INTERVAL_MSEC = 600;
+
     private String commandLine = "";
+
+    private long lastToggleTime = System.currentTimeMillis();
+
+    private char currentCursorChar = CURSOR_CHAR;
 
     @Override
     public String[] toStringArray() {
-        return new String[]{commandLine};  //To change body of implemented methods use File | Settings | File Templates.
+
+        if (System.currentTimeMillis() - lastToggleTime > CURSOR_BLINK_INTERVAL_MSEC) {
+            toggleCursor();
+        }
+
+        return new String[]{commandLine + currentCursorChar};  //To change body of implemented methods use File | Settings | File Templates.
     }
+
+
+    private void toggleCursor() {
+        currentCursorChar = (currentCursorChar == CURSOR_CHAR ? ' ' : CURSOR_CHAR);
+        lastToggleTime = System.currentTimeMillis();
+    }
+
 
     public void onChar(char ch) {
         if (isPrintableChar(ch)) {
