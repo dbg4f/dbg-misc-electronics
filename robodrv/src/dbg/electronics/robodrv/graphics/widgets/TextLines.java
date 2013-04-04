59d0554e15c33d0f561be10efe76e161107901ab
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-04-04 20:36:19 +0000

    Multiple values with history in one chart

--- robodrv/src/dbg/electronics/robodrv/graphics/widgets/TextLines.java
+++ robodrv/src/dbg/electronics/robodrv/graphics/widgets/TextLines.java
@@ -1,13 +1,19 @@
 package dbg.electronics.robodrv.graphics.widgets;
 
 import dbg.electronics.robodrv.graphics.DashboardWidget;
+import dbg.electronics.robodrv.graphics.GraphicConstants;
 import dbg.electronics.robodrv.head.MultilineReportable;
 
 import java.awt.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class TextLines implements DashboardWidget {
 
+    public static final Color DEFAULT_TEXT_COLOR = Color.WHITE;
     private MultilineReportable multilineReportable;
+
+    private Color[] colors = GraphicConstants.COMMON_COLOR_ARRAY;
 
     private int x, y;
 
@@ -19,6 +25,10 @@ public class TextLines implements DashboardWidget {
         this.multilineReportable = multilineReportable;
     }
 
+    public void setColors(Color... colors) {
+        this.colors = colors;
+    }
+
     public void setX(int x) {
         this.x = x;
     }
@@ -27,21 +37,35 @@ public class TextLines implements DashboardWidget {
         this.y = y;
     }
 
+    private Color getRowColor(int rowIndex) {
+        if (colors != null && colors.length > rowIndex) {
+            return colors[rowIndex];
+        }
+        else {
+            return DEFAULT_TEXT_COLOR;
+        }
+    }
+
     @Override
     public void onDraw(Graphics2D g2) {
 
-        g2.setColor(Color.WHITE);
+        g2.setColor(DEFAULT_TEXT_COLOR);
 
         g2.setFont(font);
 
         int currentY = y;
 
+        int i=0;
+
         for (String line : multilineReportable.toStringArray()) {
+
+            g2.setColor(getRowColor(i));
 
             g2.drawString(line, x, currentY);
 
             currentY += fontSize * 1.2;
 
+            i++;
         }
 
     }
