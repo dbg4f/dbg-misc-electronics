ebe7ba968ce29fc790a4aeca4dc5619452cd47fe
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-09-19 20:19:59 +0000

    ADC logs to csv

--- trunk/robodrv/src/dbg/electronics/robodrv/groovy/reg.groovy
+++ trunk/robodrv/src/dbg/electronics/robodrv/groovy/reg.groovy
@@ -1,13 +1,17 @@
 t.write("DDRD",   "00110000")
 t.write("TCCR1A", "10100001")
 t.write("TCCR1B", "00001001")
-t.write("OCR1AH", "0000000")
-t.write("OCR1AL", "1000000")
-t.write("OCR1BH", "0000000")
-t.write("OCR1BL", "1000000")
+t.write("OCR1AH", "00000000")
+t.write("OCR1AL", "10000000")
+t.write("OCR1BH", "00000000")
+t.write("OCR1BL", "10000000")
+t.write("DDRB",   "00011000")
 
 
 
 //drive
 t.write("OCR1AL", "10000000")
 t.write("PORTB",  "00010000")
+
+// OCR1A, PB.4 - front
+// OCR1B, PB.3 - back
