ee14b1e3f5f4aeb65218adcf5c313e8ea18e82b6
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-10-03 19:09:28 +0000

    command line evaluation with groovy

---
+++ robodrv/src/dbg/electronics/robodrv/groovy/Functions.java
@@ -0,0 +1,22 @@
+package dbg.electronics.robodrv.groovy;
+
+
+import groovy.lang.Script;
+
+public class Functions extends Script {
+
+    public String time11() {
+
+        //String t = new Date().toString();
+
+        //System.out.println("t = " + t);
+
+        return "11";
+    }
+
+
+    @Override
+    public Object run() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
