8641f5c6f112eb11a37e57178184f70fd80b3924
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2013-10-18 19:30:45 +0000

    emulator ADC feed

--- robodrv/src/dbg/electronics/robodrv/emulator/EmulatorLauncher.java
+++ robodrv/src/dbg/electronics/robodrv/emulator/EmulatorLauncher.java
@@ -20,12 +20,46 @@ public class EmulatorLauncher {
             McEmulator emulator = new McEmulator(clientSocket.getInputStream(), clientSocket.getOutputStream());
 
             try {
+                launchAdc(emulator);
                 emulator.main();
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
         } while (!Thread.currentThread().isInterrupted());
+
+
+    }
+
+    static void launchAdc(final McEmulator emulator){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (!Thread.currentThread().isInterrupted()) {
+
+                    for (int i=0; i<255; i++) {
+
+                        try {
+                            emulator.sendAdc(new int[]{i, i, i, i});
+                        } catch (IOException e) {
+                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        }
+
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                        }
+
+                    }
+
+
+                }
+
+            }
+        }).start();
 
 
     }
