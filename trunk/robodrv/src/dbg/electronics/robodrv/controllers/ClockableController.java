48f285d13f7bba3aaa6d76141a11568de491bb91
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2014-01-02 21:19:06 +0000

    Steering with PID, ClockableController introduced

--- trunk/robodrv/src/dbg/electronics/robodrv/controllers/ClockableController.java
+++ trunk/robodrv/src/dbg/electronics/robodrv/controllers/ClockableController.java
@@ -5,40 +5,65 @@ import dbg.electronics.robodrv.drive.MotorDrive;
 public class ClockableController {
 
     public static final int CLOCK_PER_POSITION_TIMEOUT = 20;
+    public static final int MAX_REV_COUNT = 3;
 
     private MotorDrive motorDrive;
 
     private int savedCommand;
     private int timeoutCounter;
+    private int reverseCounter;
+    private int savedError;
 
+    public void setMotorDrive(MotorDrive motorDrive) {
+        this.motorDrive = motorDrive;
+    }
 
+    private void resetCounters(int currentError) {
 
+        timeoutCounter = Math.abs(currentError) * CLOCK_PER_POSITION_TIMEOUT;
+
+        reverseCounter = MAX_REV_COUNT;
+
+    }
+
+    private boolean isCommandChanged(int command) {
+        return command != savedCommand;
+    }
+
+    private boolean isReversed(int currentError) {
+        return (currentError > 0) ^ (savedError > 0);
+    }
 
     public void onClock(int commandPosition, int actualPosition) throws Exception {
 
         int currentError = commandPosition - actualPosition;
 
-        int errorSize = Math.abs(currentError);
+        if (isCommandChanged(commandPosition)) {
 
-        if (commandPosition != savedCommand) {
-
-            timeoutCounter = errorSize * CLOCK_PER_POSITION_TIMEOUT;
+            resetCounters(currentError);
 
             savedCommand = commandPosition;
         }
 
 
-        if (timeoutCounter <= 0) {
+        if (timeoutCounter <= 0 || reverseCounter <= 0) {
             motorDrive.setPwm(0);
         }
         else {
 
+
+
+
             timeoutCounter--;
+
+            if (isReversed(currentError)) {
+                reverseCounter--;
+            }
+
         }
 
 
-
-
+        savedError = currentError;
 
 
 
