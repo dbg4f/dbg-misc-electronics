5c2ba2e565e9df79352e7720fa7d3209fff7a0bb
dmitri.bogdel@gmail.com <dmitri.bogdel@gmail.com@21bd61a7-d6fa-292f-a732-182eb46d76ed>
2014-01-26 14:45:51 +0000

    Initiated moving PID to MC

--- src_c/avr/lufa/avrpid.c
+++ src_c/avr/lufa/avrpid.c
@@ -3,21 +3,70 @@
 
 #define PID_CHANNEL 1
 
+// TODO: ~1 second
+#define REG_TIMEOUT_PER_CYCELE 100 
+
+#define MAX_REVERSALS_PER_CYCLE 3
+
 uint8_t currentCommand = 0;
+uint8_t savedPwm = 0;
+
+int16_t timeoutCounter = REG_TIMEOUT_PER_CYCELE;
+int8_t reverseCounter = MAX_REVERSALS_PER_CYCLE;
+
+
+void set_pwm(uint8_t value) 
+{	
+	DRV_setPwm(PID_CHANNEL, value);	
+	savedPwm = value;
+}
 
 void AVRPID_onClock(uint8_t currentPosition)
 {
 	// NB! beware of ISR restrictions
 
+	int16_t currentError = currentCommand - currentPosition;
+		
+	if (timeoutCounter <= 0 || reverseCounter <= 0) 
+	{
+		if (savedPwm != 0) 
+		{
+            set_pwm(0);
+        }
+    }
+    else 
+    {
+		
+	}	
+
 	
 	
-	DRV_setPwm(PID_CHANNEL, 0);
+	
 	
 
 }
 
+
+void reset_counters(void)
+{
+	timeoutCounter = REG_TIMEOUT_PER_CYCELE;
+	reverseCounter = MAX_REVERSALS_PER_CYCLE;
+}
 
 void AVRPID_setCommand(uint8_t command)
 {
+	
+	if (command != currentCommand)
+	{
+		reset_counters();
+	}
+	
 	currentCommand = command;
+	
 }
+
+void AVRPID_setupTimer(void)
+{
+	
+}
+
